ff1d647457b1b7d59fc8cca6e6daeb9ec5cb4e28
Janusz <JanuszAGH@users.noreply.github.com>
2019-10-08 23:17:57 +0200

    Inheritance fixed parameterized tests (#12)
    * initial commit with gitignore
    
    * update gitignore
    
    * Car
    
    * update gitignore
    
    * update gitignore
    
    * update Car
    
    * Add color property tu car
    
    * Clean up
    
    * finish lesson 4
    
    * finish lesson 4
    
    * finish lesson 4
    
    * inheritance
    
    * student
    
    * student finished, basic version of tests
    
    * student finished, parameterize version of tests
    
    * student finished, parameterize version of tests
    
    * student finished, parameterize version of tests
    
    * student finished, parameterize version of tests
    
    * inheritance
    
    * student
    
    * student finished, basic version of tests
    
    * student finished, parameterize version of tests
    
    * student finished, parameterize version of tests
    
    * student finished, parameterize version of tests
    
    * student finished, parameterize version of tests
    
    * student finished, parameterize version of tests
    
    * student finished, parameterize version of tests

--- src/main/java/net/forprogrammers/ICreateStudent.java
+++ src/main/java/net/forprogrammers/ICreateStudent.java
@@ -2,9 +2,6 @@ package net.forprogrammers;
 
 import java.util.List;
 
-/**
- * Created by Janusz Kacki on 08/10/2019. Project; bielmarcus
- */
 public interface ICreateStudent extends IStudent {
 
     List<IStudent> getStudents();
