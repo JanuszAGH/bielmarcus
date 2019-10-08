58ed8cdd080dc1c94132895d3b846a1a419a439c
janusz <janusz.agh@gmail.com>
2019-10-08 11:10:21 +0200

    update Car

--- src/main/java/pl/janusz/Car.java
+++ src/main/java/pl/janusz/Car.java
@@ -5,4 +5,76 @@ package pl.janusz;
  */
 public class Car {
 
+    private String make;
+    private String model;
+    private int year;
+
+    public Car(String make, String model, int year) {
+
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Car car = (Car) o;
+
+        if (getYear() != car.getYear()) return false;
+        if (!getMake().equals(car.getMake())) return false;
+        return getModel().equals(car.getModel());
+    }
+
+    @Override
+    public int hashCode() {
+
+        int result = getMake().hashCode();
+        result = 31 * result + getModel().hashCode();
+        result = 31 * result + getYear();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Car{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                '}';
+    }
+
+    public String getMake() {
+
+        return make;
+    }
+
+    public void setMake(String make) {
+
+        this.make = make;
+    }
+
+    public String getModel() {
+
+        return model;
+    }
+
+    public void setModel(String model) {
+
+        this.model = model;
+    }
+
+    public int getYear() {
+
+        return year;
+    }
+
+    public void setYear(int year) {
+
+        this.year = year;
+    }
 }
