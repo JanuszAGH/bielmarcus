f424d7805488bf197b8f689b0e6b15b797c8d68e
Janusz <JanuszAGH@users.noreply.github.com>
2019-10-13 17:19:42 +0200

    Lesson17 (#17)
    * initial commit with gitignore
    
    * update gitignore
    
    * Car
    
    * update gitignore
    
    * update gitignore
    
    * update Car
    
    * Add color property tu car
    
    * Clean up
    
    * finish lesson 4
    
    * finish lesson 4
    
    * finish lesson 4
    
    * remove redundand Person test
    
    * Lesson 17 finished

---
+++ src/main/java/pl/janusz/lesson17/Animal.java
@@ -0,0 +1,22 @@
+package pl.janusz.lesson17;
+
+/**
+ * Created by Janusz Kacki on 13/10/2019. Project; bielmarcus
+ */
+public class Animal {
+
+    public static void hello() {
+
+        System.out.println("Howdy buddy");
+    }
+
+    public void eat() {
+
+        System.out.println("Animal is eating");
+    }
+
+    public void age() {
+
+        System.out.println("Age is calculated...");
+    }
+}
---
+++ src/test/java/pl/janusz/lesson17/ZooTest.java
@@ -0,0 +1,24 @@
+package pl.janusz.lesson17;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * Created by Janusz Kacki on 13/10/2019. Project; bielmarcus
+ */
+public class ZooTest {
+
+    @Test
+    public void shouldfeedAllAnimals() {
+
+        Zoo zoo = new Zoo();
+        Animal[] animals = new Animal[]{new Dog(), new Gorilla(), new Lion(),new Tiger()};
+
+        for (Animal animal : animals) {
+            animal.he
+        }
+    }
+}
